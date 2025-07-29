b448898fcb58e802889685ffb504ddd26b234c7d
Alisher <davincimtn@gmail.com>
2025-07-29 15:24:16 +0500

    Исправил путь к стилям

--- landing/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.accordion__heading').forEach(heading => {
-  heading.addEventListener('click', () => {
-    const item = heading.parentElement;
-    item.classList.toggle('active');
-  });
-});
